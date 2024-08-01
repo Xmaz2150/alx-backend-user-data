b5b405c6aba19958cbcaaefcce2519c7a272b04a
tAf <muzirat0@gmail.com>
2024-08-01 12:00:27 +0000

    secure formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,12 @@
 """
 logging module
 """
+
 import re
+import logging
 from typing import List
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -13,4 +17,25 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     for f in fields:
         message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        sucure formatter
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
 
