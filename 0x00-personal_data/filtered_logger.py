1ec127f9715435f339f90911a74e06cadcbc6530
tAf <muzirat0@gmail.com>
2024-08-01 11:27:11 +0000

    regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+logging module
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    obfusicate PII
+    """
+    for f in fields:
+        message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
+    return message
+
