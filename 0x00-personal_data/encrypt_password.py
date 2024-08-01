5c3a01f1a75d4c84a5180b93b1e56c333304a1fe
tAf <muzirat0@gmail.com>
2024-08-01 16:01:35 +0000

    password check

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     encrypts password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    password validator
+    """
+    new_hash = bcrypt.hashpw(password.encode(),  hashed_password)
+    return new_hash == hashed_password
