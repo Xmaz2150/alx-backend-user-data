5b67c6ff800195acf353a246e332b92593338526
tAf <muzirat0@gmail.com>
2024-08-06 12:16:16 +0200

    0. api test 1 - 2. unauthorized and forbidden handlers 3. Auth class: 	require_auth 	authorization_header 	current_user
    4. require_auth update:
    	defines which routes don't need authentication
    
    5. authorization_header update:
    	request validation
       auth instance with `auth type`
       house keeping before each request

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Authentication mdule
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if path is not in excluded paths
+        """
+        if path and excluded_paths:
+            if path[-1] != '/':
+                path += '/'
+
+            if path not in excluded_paths:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Checks authorization hearder in request
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
