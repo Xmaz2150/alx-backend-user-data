0dcbdaae56731210fa7dbaab40862b200a18804e
tAf <muzirat0@gmail.com>
2024-08-09 01:22:23 +0200

    3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,10 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[session_id] = user_id
 
             return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns user id based on session id
+        """
+        if session_id and isinstance(session_id, str):
+            return self.user_id_by_session_id.get(session_id)
