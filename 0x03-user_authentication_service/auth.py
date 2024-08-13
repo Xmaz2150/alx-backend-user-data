5f0dd69ba60531b2fba0dad93f2bb34909687149
tAf <muzirat0@gmail.com>
2024-08-13 13:23:00 +0200

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,10 @@
 Auhentication module
 """
 import bcrypt
+from db import DB
+from user import User
+from typing import TypeVar
+from sqlalchemy.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +14,18 @@ def _hash_password(password: str) -> bytes:
     Encrypts password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """
+        Registers user to DataBase
+        """
+        DB = self._db
+        DB.add_user(email, _hash_password(password))
