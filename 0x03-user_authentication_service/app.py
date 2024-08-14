b0f4506c909a9f9df5f3b74eceba7a70b1922bac
tAf <muzirat0@gmail.com>
2024-08-14 15:08:21 +0200

    11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask entry point
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -31,6 +31,23 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    resp = make_response(jsonify({"email": f"{email}", "message": "logged in"}))
+    resp.set_cookie('session_id', session_id)
+
+    return resp
 
 
 if __name__ == "__main__":
