559fff5bc263fe3a211aefd38e0d27ef08d1a336
tAf <muzirat0@gmail.com>
2024-08-14 16:15:45 +0200

    15. User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,12 +58,26 @@ def login() -> str:
 @app.route('/sessions', methods=['DELETE'])
 def logout() -> str:
     """
+    Logs ser out
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """
+    Simple user profile
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": f"{user.email}"}), 200
     else:
         abort(403)
 
