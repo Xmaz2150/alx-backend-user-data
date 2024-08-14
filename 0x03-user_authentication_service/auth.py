b72072f681ec031b9da8321981da4b8b69879a08
tAf <muzirat0@gmail.com>
2024-08-14 10:17:04 +0200

    5. Register user update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,11 +27,12 @@ class Auth:
         """
         Registers user to DataBase
         """
+        DB = self._db
         try:
-            if self._db.find_user_by(email=email):
-                raise ValueError('User {} already exists'.format(email))
+            user = DB.find_user_by(email=email):
+            raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
+            return DB.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
         """
@@ -39,6 +40,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
