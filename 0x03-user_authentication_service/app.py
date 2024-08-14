308077e3b31b43d1513dbb792698a91a016deae6
tAf <muzirat0@gmail.com>
2024-08-14 16:05:09 +0200

    14. Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask entry point
 """
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
@@ -53,6 +53,19 @@ def login() -> str:
     resp.set_cookie('session_id', session_id)
 
     return resp
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
